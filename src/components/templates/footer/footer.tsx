0a40a6200dcea49943bed86cfe24fa3322ce7715
Philip Mathieu <philip.eng.mathieu@gmail.com>
2024-02-19 17:21:49 -0500

    Add social icons and update header/footer

--- src/components/templates/footer/footer.tsx
+++ src/components/templates/footer/footer.tsx
@@ -1,5 +1,5 @@
 import { useTranslation } from 'next-i18next';
-import Link from 'next/link';
+import { SocialIcon } from 'react-social-icons';
 
 import { Container } from '@src/components/shared/container';
 
@@ -8,9 +8,34 @@ export const Footer = () => {
 
   return (
     <footer className="border-t-color mt-10 border-t border-gray200">
-      <Container className="py-8">
+      <Container className="flex-items-center flex justify-between py-8">
         <h2 className="h4 mb-4">{t('footer.aboutUs')}</h2>
-        <div className="max-w-4xl">{t('footer.description')}</div>
+        <div className="max-w-4xl">
+          I am a data science graduate student at The Roux Institute, Northeastern University&apos;s
+          campus in Portland, ME. I am seeking full-time opportunities starting in late spring 2024.
+          To get a sense of my project experience and skills, please refer to{' '}
+          <a href="https://github.com/PhilipMathieu" target="_blank" rel="noopener noreferrer">
+            my GitHub profile
+          </a>
+          . For a broader sense of my professional experience,{' '}
+          <a href="https://linkedin.com/in/PhilipMathieu" target="_blank" rel="noopener noreferrer">
+            connect with me on LinkedIn
+          </a>
+          .
+        </div>
+        <div className="flex justify-end">
+          <SocialIcon
+            url="https://github.com/PhilipMathieu"
+            className="mr-2"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+          <SocialIcon
+            url="https://linkedin.com/in/PhilipMathieu"
+            target="_blank"
+            rel="noopener noreferrer"
+          />
+        </div>
       </Container>
     </footer>
   );
--- src/components/templates/header/header.tsx
+++ src/components/templates/header/header.tsx
@@ -1,5 +1,6 @@
 import { useTranslation } from 'next-i18next';
 import Link from 'next/link';
+import { SocialIcon } from 'react-social-icons';
 
 import { Container } from '@src/components/shared/container';
 
@@ -11,8 +12,21 @@ export const Header = () => {
       <nav>
         <Container className="flex items-center justify-between">
           <Link href="/" title={t('common.homepage')} className="">
-            <h1>PEM Blog</h1>
+            <h1>Philip Englund Mathieu</h1>
           </Link>
+          <div className="flex justify-end">
+            <SocialIcon
+              url="https://github.com/PhilipMathieu"
+              className="mr-2"
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+            <SocialIcon
+              url="https://linkedin.com/in/PhilipMathieu"
+              target="_blank"
+              rel="noopener noreferrer"
+            />
+          </div>
         </Container>
       </nav>
     </header>
