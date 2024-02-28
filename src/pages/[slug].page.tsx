c20f67d1b84031c4dd68ccdbc0652650c4050a03
Philip Mathieu <philip.eng.mathieu@gmail.com>
2024-02-28 02:50:04 -0500

    Eliminate ghost "0"

--- src/pages/[slug].page.tsx
+++ src/pages/[slug].page.tsx
@@ -24,10 +24,10 @@ const Page = (props: InferGetStaticPropsType<typeof getStaticProps>) => {
       <Container>
         <ArticleHero article={blogPost} isFeatured={props.isFeatured} isReversedLayout={true} />
       </Container>
-      <Container className="mt-8 max-w-4xl">
+      <Container className="mt-8 max-w-5xl">
         <ArticleContent article={blogPost} />
       </Container>
-      {relatedPosts?.length && (
+      {relatedPosts?.length > 0 && (
         <Container className="mt-8 max-w-5xl">
           <h2 className="mb-4 md:mb-6">{t('article.relatedArticles')}</h2>
           <ArticleTileGrid className="md:grid-cols-2" articles={relatedPosts} />
